85385e7fab13828bdec1b05ec4e47b96c58e34b1
Rinat Abdullin <rinat.abdullin@gmail.com>
2015-05-13 12:16:53 +0500

    importing first bits

---
+++ init.el
@@ -0,0 +1,33 @@
+;;(defun fontify-frame (frame)
+;;  (set-frame-parameter frame 'font "Monospace-11"))
+;; Increase size
+;;(set-face-attribute 'default nil :height 160)
+
+(setq emacs-root-dir user-emacs-directory)
+(add-to-list 'load-path (concat user-emacs-directory
+        (convert-standard-filename "lisp/")))
+
+
+(load "functions")
+(load "env")
+;; loads all components
+(load "packages")
+
+
+(load "ui")
+
+;; various key bindings
+(load "my-chords")
+(load "window")
+(load "my-evil")
+(load "editor")
+
+;; modes
+(load "my-org-mode")
+(load "my-js-mode")
+(load "my-web-mode")
+(load "my-doc-mode")
+(load "my-go-mode")
+(load "my-csharp-mode")
+
+(server-start)
