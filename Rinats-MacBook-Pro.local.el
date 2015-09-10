1390aaf3fcac865fd48f985324528075e2f90a39
Rinat Abdullin <rinat.abdullin@gmail.com>
2015-09-11 00:15:41 +0500

    update erlang path to the latest brew

--- Rinats-MacBook-Pro.local.el
+++ Rinats-MacBook-Pro.local.el
@@ -1,1 +1,2 @@
-(setq ra/erlang-path "/usr/local/Cellar/erlang/17.5/")
+(setq ra/erlang-root-path "/usr/local/Cellar/erlang/18.0.3/")
+(setq ra/erlang-tools-path "/usr/local/Cellar/erlang/18.0.3/lib/erlang/lib/tools-2.8/emacs/")
--- build/init-erlang.el
+++ build/init-erlang.el
@@ -1,9 +1,10 @@
 (when
-    (boundp 'ra/erlang-path)
+    (boundp 'ra/erlang-root-path)
 
-  (setq erlang-root-dir (concat ra/erlang-path "/lib/erlang/lib"))
-  (setq erlang-emacs-path (concat ra/erlang-path "/lib/erlang/lib/tools-2.7.2/emacs"))
-  (setq erlang-bin-path (concat ra/erlang-path "/lib/erlang/bin"))
+  (setq erlang-root-dir (concat ra/erlang-root-path "/lib/erlang/lib"))
+  (setq erlang-bin-path (concat ra/erlang-root-path "/lib/erlang/bin"))
+  (setq erlang-emacs-path ra/erlang-tools-path)
+
   (setq load-path (cons erlang-emacs-path load-path))
   (setq exec-path (cons erlang-bin-path exec-path))
   (require 'erlang-start)
