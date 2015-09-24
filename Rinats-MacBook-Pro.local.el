8b81a8e845766798de39f74ba77d8b9e35f182b0
Rinat Abdullin <rinat.abdullin@gmail.com>
2015-09-24 17:11:48 +0500

    added local path to R

--- Rinats-MacBook-Pro.local.el
+++ Rinats-MacBook-Pro.local.el
@@ -1,2 +1,3 @@
 (setq ra/erlang-root-path "/usr/local/Cellar/erlang/18.0.3/")
 (setq ra/erlang-tools-path "/usr/local/Cellar/erlang/18.0.3/lib/erlang/lib/tools-2.8/emacs/")
+(setq inferior-R-program-name "/usr/local/Cellar/r/3.2.2_1/bin/R")
