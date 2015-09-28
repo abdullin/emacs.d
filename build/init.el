0fb9c0f0bf81372b8153426e07d52ee8bcc31bbd
Rinat Abdullin <rinat.abdullin@gmail.com>
2015-09-28 13:35:20 +0500

    cleaning up roles

---
+++ build/init.el
@@ -0,0 +1,9 @@
+(setq ra/roles '(
+                 "org"
+                 ;; "ess"
+                 ;; "erlang"
+                 ;; "go"
+                 ;; "web" - HTML,CSS,JS,JSX etc
+                 ))
+(load-file "~/.emacs.d/build/init-main.el")
+(server-start)
