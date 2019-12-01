ecfeccccc1e17482a6df2e733ebfda09ca656dda
Rinat Abdullin <rinat.abdullin@gmail.com>
2019-12-01 22:41:17 +0500

    Add entire elpa

---
+++ elpa/multi-compile-20160306.1423/multi-compile-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "multi-compile" "20160306.1423" "Multi target interface to compile." '((emacs "24") (dash "2.12.1")) :url "https://github.com/ReanGD/emacs-multi-compile" :keywords '("tools" "compile" "build"))
